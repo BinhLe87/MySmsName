ebc3fb4aa312bb9632d493550e31541d7b281bb2
Bear Baby <phuongthule11@gmail.com>
2016-06-17 15:46:32 +0700

    Initial Commit

---
+++ MySMSBrandName/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MySMSBrandName
+//
+//  Created by Le Van Binh on 6/17/16.
+//  Copyright © 2016 Le Van Binh. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ MySMSBrandName/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MySMSBrandName
+//
+//  Created by Le Van Binh on 6/17/16.
+//  Copyright © 2016 Le Van Binh. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
